25bfcf0ec24a468c660e08f1abb86611809b3f06
liwenjie <liwenjie2@jk.pingan-inc.cn>
2017-08-23 19:04:33 +0800

    fetch

--- src/root.js
+++ src/root.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 import App from './pages/App/app';
 
 export default class Root extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+    componentWillMount() {
+        (async() => {
+            const res = await fetch(`http://localhost:3000/getData?name=${encodeURIComponent('李文杰')}`);
+            const json = await res.json();
+            this.setState({data: json.data})
+            console.log(json.data);
+        })();
+    }
+
     render() {
+
         return (
             <App>
-                <Text>
-                    12324
-                </Text>
+                {this
+                    .state
+                    .data
+                    .map(v => {
+                        return (
+                            <View>
+                                <Text>{v.work}</Text>
+                                <Text>{v.day}</Text>
+                                <Text>{v.date}</Text>
+                            </View>
+                        );
+                    })}
             </App>
         )
     }
